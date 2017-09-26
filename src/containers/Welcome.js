b1b66a745584e42a01c5ef0458fc894cde6e7270
Devin Chen <d3vin.chen@gmail.com>
2017-09-26 16:14:06 +0800

    update react-router to v4

--- src/containers/Welcome.js
+++ src/containers/Welcome.js
@@ -3,7 +3,7 @@
  */
 
 import React from 'react';
-import { Link } from 'react-router';
+import { Link } from 'react-router-dom';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
